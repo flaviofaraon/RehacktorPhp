a100430959b396607da8cfd726ce6e5b55af61b7
Flavio Faraone <f.faraone@be-tse.it>
2021-12-30 00:04:14 +0100

    ripresa + navbar e footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Footer from './Components/UI/Footer/Footer';
+import Navbar from './Components/UI/Navbar/Navbar';
 import Home from './Components/Wiews/Home/Home';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Home />
       <p>ok</p>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/Components/UI/Footer/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return (
+        <div className="container-fluid bg-danger">
+            <p>
+                questo è il footer
+            </p>
+        </div>
+    )
+}
